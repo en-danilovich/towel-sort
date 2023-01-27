5ea24c3bd1416639c1b93a5ddd146cc6dae2523f
Eugene Danilovich <bluerayend@gmail.com>
2023-01-27 22:42:09 +0300

    init: start towel sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    return matrix.reduce((accumulator, item, index) =>
+        accumulator.concat((index % 2 != 0) ? item.reverse() : item), []);
 }
